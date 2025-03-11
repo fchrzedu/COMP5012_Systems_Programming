ac1f77c1f60f38bb5f057f1bc72913c129f018e4
fchrzedu <19260104@brookes.ac.uk>
2025-03-11 10:50:21 +0000

    Added sleep to prog3

--- Part 3/prog3.c
+++ Part 3/prog3.c
@@ -8,18 +8,19 @@ int main(){
     pid_t pid = fork();
     int stat; // capture
 
-    if(pid<0){perror("->Forking has failed!\n");return 1;}
+    if(pid<0){perror(">Forking has failed!\n");return 1;}
     else if(pid == 0){//child
-        printf("->This is the child process. Hello!\n");
-        exit(2); // arbitary integer for an exit status              
+        printf(">This is the child process. Hello and goodnight!\n");sleep(5);         
+        printf(">This is the child process. I have woken up, exiting now...\n");
+        exit(200); // arbitary integer for an exit status              
     }
-    else{//parent
+    else{ //parent
         wait(&stat);
-        if(WIFEXITED(stat)){
-            printf("->This is the parent process. Child has exited with status: %d\n",WEXITSTATUS(stat)); // check whether child exited, and print its status
+        if(WIFEXITED(stat)){ // check whether child has exited. If so, capture its exit status
+            printf(">This is the parent process. Child has exited with status: %d\n",WEXITSTATUS(stat)); 
         }    
     }
-    printf("Bye\n");
+    printf(">Bye!\n");
 
     
 
