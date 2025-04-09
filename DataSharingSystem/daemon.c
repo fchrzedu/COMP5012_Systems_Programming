7ccb6bbf61ad0d45d34c462799bf6e94a9a3059d
fchrzedu <19260104@brookes.ac.uk>
2025-04-09 13:36:35 +0100

    Failed doing UNIX domain socket

--- DataSharingSystem/daemon.c
+++ DataSharingSystem/daemon.c
@@ -4,7 +4,10 @@
 - Coursework wants 'daemon that LISTENS', therefore server-side resides here
 - Client calls sharing lib, that sockes into daemon w/ server.
 */
-
+/*
+* lpedated.c - Simple timestamping daemon. From
+* Linux Programming by Example by Kurt Wall
+*/
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdio.h>
@@ -17,15 +20,17 @@
 #include <syslog.h>
 /* Socket headers \/*/
 #include <sys/socket.h>
-#include <sys/un.h> /* for UNIX domain, sysnet used for TCP*/
-
-#define SOCKETPATH "/tmp/daemon_socket_data"
+#include <netinet/in.h>
 
 int main(void)
 {
     pid_t pid, sid;
+    time_t timebuf;
+    int fd, len;
       
-    
+    /*
+    Identify as lpedated - start log
+    */
     openlog("daemondataserver", LOG_PID, LOG_DAEMON);
     
     /* Need a non parented PID */
@@ -52,77 +57,44 @@ int main(void)
 
     /* Reset the file mode */
     umask(0); /* Protection */
-    /* 
-    * Due to using a UNIX domain socket rather than a TCP,
-    * We have to close any terminal I/O, but re-open them not connected to terminals 
-    * Essentially redirecting what goes to stdout (1) redirects to (2) stderr
-    * */
-    close(STDIN_FILENO); 
-    close(STDOUT_FILENO); 
-    close(STDERR_FILENO); 
-
-   
-
-    /*int nullfd = open("/dev/null", O_RDWR);
-    if (nullfd == -1) {
-        syslog(LOG_ERR, "Failed to open /dev/null: %s", "devnullerr");
-        exit(EXIT_FAILURE);
-    }
-    dup2(nullfd, STDIN_FILENO);
-    dup2(nullfd, STDOUT_FILENO);
-    dup2(nullfd, STDERR_FILENO);*/
+    /* Close stdin, etc. */
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
 
     /* ----------------- Socket work below ---------------- */
 
-    /* Initialise server socket, not TCP but UNIX domain hence _un and AF_UNIX */
-    unlink(SOCKETPATH);
-    //potentially implement a clear previous sockets from path here
+    int server_sock, client_sock; /* Socket descriptors */
+    /* Create socket
+    - INET = IPv4 address, STREAM = TCP stream, 0 = automatic protocol */
+    server_sock = socket(AF_INET, SOCK_STREAM, 0); /* Create socket */
 
-    int client_sock, server_sock ; /* socket descriptors */
-    struct sockaddr_un server_address;
-    server_sock = socket(AF_UNIX, SOCK_STREAM, 0);
-    if(server_sock<0){
-        syslog(LOG_ERR,"Server socket creation failed!\n");
-        exit(EXIT_FAILURE);
-    }
+    struct sockaddr_in server_address; /* Server address structure */
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9002); // port num
+    server_address.sin_addr.s_addr = INADDR_ANY; // accept any connections
 
-    /*
-    1. Clears server_address memory to zero
-    2. Ensure family addressing is for UNIX, not TCP
-    3. Copy the socket path into sun_path, where -1 represents \0
-    */
-    memset(&server_address, 0, sizeof(server_address));
-    server_address.sun_family = AF_UNIX; /* Stores path on sys as 'server IP' */
-    strncpy(server_address.sun_path, SOCKETPATH, sizeof(server_address.sun_path) - 1);
+    /* Binds serversock to address, associated with IPv4 address and its length */
+    bind(server_sock, (struct sockaddr*) &server_address, sizeof(server_address));
 
-    /* Bind socket to path and daemon */
-    if (bind(server_sock, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) {
-        syslog(LOG_ERR, "%s\n", "binderr");
-        close(server_sock); /* Close server socke and exit with fail, loggin err */
-        exit(EXIT_FAILURE);
-    }
+    /* Listen for incoming connections, 1 connection at a time */
+    listen(server_sock, 1);
 
-    listen(server_sock,1);
-    while(1){
-        client_sock = accept(server_sock,NULL,NULL);
-        if(client_sock <0){
-            syslog(LOG_ERR, "%s\n", "accepterr");
-            //close(client_sock); /* Close client socket after fail */
-            //exit(EXIT_FAILURE);
+    while(1){ /* Infinite loop, to constantly listen for new connections */  
+        client_sock = accept(server_sock, NULL, NULL); /* Accept connection */
+        if(client_sock < 0)        {
+            syslog(LOG_ERR, "%s\n", "accept"); 
+            exit(EXIT_FAILURE);
         }
-
-        const char *welcome_msg = "---You have succesfully connected to the daemon!\n";
-        send(client_sock,welcome_msg,strlen(welcome_msg),0);
+        send(client_sock, "---Waiting on connection---\n", 30, 0); /* Send message to syslog */
+        send(client_sock, "---Hello you have connected to the daemon!\n---",43,0 );
+        close(server_sock);
         close(client_sock);
     }
-    close(server_sock);
-    unlink(SOCKETPATH);
+    
+    
     closelog();
     exit(EXIT_SUCCESS);
-    
-
-
-    return 0;
 }
   
   
