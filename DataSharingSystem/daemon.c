25532863397f7ff5d17af006c339238cd74f19ac
fchrzedu <19260104@brookes.ac.uk>
2025-04-12 12:13:37 +0100

    Fully doing TCP, cannot figure out SIGSEGV error

--- DataSharingSystem/daemon.c
+++ DataSharingSystem/daemon.c
@@ -1,62 +1,115 @@
-#include <sys/socket.h>
-#include <sys/un.h>
+/*
+- Daemon for the program.
+- It contains server side socket functionalities, a seperate client program exist
+- Coursework wants 'daemon that LISTENS', therefore server-side resides here
+- Client calls sharing lib, that sockes into daemon w/ server.
+
+
+- SIGISEV & memory dumping errors when using UNIX domain socket - had to use TCP with a loopback
+*/
+
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <unistd.h>
+#include <time.h>
+#include <syslog.h>
+/* Socket headers \/*/
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-#define SOCKET_PATH "/tmp/testsocket.sock"
+#define PORT 9002
+#define IP_ADDY "127.0.0.1"
 
-int main() {
-    int server_fd, client_fd;
-    struct sockaddr_un server_addr;
 
-    // Cleanup old socket if it exists
-    unlink(SOCKET_PATH);
+void daemon_make(){
+    pid_t pid, sid;   
+    
+    openlog("daemon_TCP_server", LOG_PID, LOG_DAEMON);
+    
+    pid = fork();
+    if(pid < 0) { 
+        syslog(LOG_ERR, "%s\n", "[-]Error forking\n"); 
+        exit(EXIT_FAILURE);
+    }
+    if(pid > 0){exit(EXIT_SUCCESS);} /* Parent exists */      
+    syslog(LOG_INFO, "[+]Succesfully forked()\n");
+        
+        
+    /* ------------In the child (orphaned)...------------- */
 
-    // Create UNIX domain socket
-    server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (server_fd < 0) {
-        perror("socket");
+    if((sid = setsid()) < 0) { /* Create new unique session */
+        syslog(LOG_ERR, "%s\n", "[-]Setsid error\n"); /* Log error if failed to setsid */
+        exit(EXIT_FAILURE);
+    }
+   
+    /* Change to root directory  */
+    if((chdir("/")) < 0) {
+        syslog(LOG_ERR, "%s\n", "[-]Chdir error\n");
         exit(EXIT_FAILURE);
     }
 
-    // Clear and set up the socket address
-    memset(&server_addr, 0, sizeof(struct sockaddr_un));
-    server_addr.sun_family = AF_UNIX;
-    strncpy(server_addr.sun_path, SOCKET_PATH, sizeof(server_addr.sun_path) - 1);
-
-    // Correctly calculate bind length
-    int bind_len = sizeof(server_addr.sun_family) + strlen(server_addr.sun_path);
-
-    if (bind(server_fd, (struct sockaddr *)&server_addr, bind_len) < 0) {
-        perror("bind");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
-
-    if (listen(server_fd, 1) < 0) {
-        perror("listen");
-        close(server_fd);
-        unlink(SOCKET_PATH);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("[+] Server is listening on %s\n", SOCKET_PATH);
-
-    while (1) {
-        client_fd = accept(server_fd, NULL, NULL);
-        if (client_fd < 0) {
-            perror("accept");
-            continue;
-        }
-
-        const char *msg = "---Hello from the daemon!\n";
-        write(client_fd, msg, strlen(msg));
-        close(client_fd);
-    }
-
-    close(server_fd);
-    unlink(SOCKET_PATH);
-    return 0;
+    /* Reset the file mode */
+    umask(0); /* Protection */
+    /* Close stdin, etc. */
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
 }
+void server_socket_TCP(){
+    
+    /* ----------------- Socket work below ---------------- */
+
+    int server_sock; /* Socket descriptors */
+    /* Create socket
+    - INET = IPv4 address, STREAM = TCP stream, 0 = automatic protocol */
+    server_sock = socket(AF_INET, SOCK_STREAM, 0); /* Create socket */ 
+
+    struct sockaddr_in server_address; /* Server address structure */
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT); // port num
+    server_address.sin_addr.s_addr = INADDR_ANY; // accept any connections
+
+    /* Binds serversock to address, associated with IPv4 address and its length */
+    if(bind(server_sock, (struct sockaddr*) &server_address, sizeof(server_address))){
+        syslog(LOG_ERR,"%s\n","[-]Bind() error\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    /* Listen for incoming connections, 1 connection at a time */
+    if(listen(server_sock, 2) < 0){
+        syslog(LOG_ERR, "%s\n", "[-]Listen() error\n");       
+        exit(EXIT_FAILURE);
+    }
+    
+    int client_sock;
+
+    while(1){ /* Infinite loop, to constantly listen for new connections */  
+        client_sock = accept(server_sock, NULL, NULL); /* Accept connection */
+        if(client_sock < 0)        {
+            syslog(LOG_ERR, "%s\n", "[-]Accept() error\n"); 
+            exit(EXIT_FAILURE);
+        }
+        send(client_sock, "Waiting on connection......\n", 30, 0);sleep(2);/* Send message to syslog */
+        send(client_sock, "---Hello you have connected to the daemon!\n---",43,0 );   
+        
+    }
+    close(server_sock);
+    close(client_sock);
+    closelog();
+    exit(EXIT_SUCCESS);
+}
+
+int main(){
+    daemon_make();
+    server_socket_TCP();
+    return 0;
+
+}
+
+  
+  
--- DataSharingSystem/libshare.c
+++ DataSharingSystem/libshare.c
@@ -7,37 +7,53 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <syslog.h>
+
 
 #define PORT 9002 
 #define LOOPBACK_IP "127.0.0.1"
-#define MSGSIZE 1024
 
-
-uint8_t sendNewBlock(const char *ID, const uint8_t *secret, const uint32_t data_length, const char *data)
-{ /* Creates and formats message to daemon, then sends it down socket */
-
-    int blockfd = connect_to_daemon();
-    char msg[MSGSIZE];
-    int formatted_msg;
-     
-
-
-    
-    printf("SENDING:%s%s%d")
-
-
-    uint8_t 
+void socketFail(){
+    syslog(LOG_ERR,"[-]Client socket creation failed!\n");
+        exit(EXIT_FAILURE);
+    }
+void callLog(){
+    openlog("API shared library", LOG_PID, LOG_DAEMON);
 }
-uint8_t getBlock(const char *ID, const uint8_t *secret, const uint32_t buffer_size, const char *buffer){}
+
+uint8_t sendNewBlock(const char *ID, const uint8_t *secret, const uint32_t data_length, const char *data){
+    callLog();
+    /* 
+    - ID = string of callers choice
+    - secret = 16byte array unsigned bytes of callers choice
+    - data = ptr to data structure in memory (buffer)
+    - data_length = len(data)
+    - buffer_size = allocated buffer memory (unoccupied memory)
+
+
+    - Establish IPC connection to daemon
+    - send appropriate comand to it, including ID and secret
+    - send data_length bytes starting at address data to it
+    - retrieve daemon response, indicate whether storing data was succesful
+    - return value reflecting daemon response
+    */
+    int blockfd = connect_to_daemon();
+
+    return 0;
+
+}
+uint8_t getBlock(const char *ID, const uint8_t *secret, const uint32_t buffer_size, const char *buffer){
+
+    return 0;
+}
+
 
 
 int connect_to_daemon(){
-    
+    callLog();
     int c_sock_fd = socket(AF_INET, SOCK_STREAM, 0); /* Create socket */
-    if(c_sock_fd < 0){
-        perror("[-] Client socket creation failed!\n");
-        exit(EXIT_FAILURE);
-    }
+    if(c_sock_fd < 0){socketFail();}     
+    
 
     struct sockaddr_in my;
     my.sin_family = AF_INET;
@@ -46,11 +62,11 @@ int connect_to_daemon(){
 
     /* Connect to the server */
     if (connect(c_sock_fd, (struct sockaddr*)&my, sizeof(my)) < 0) {
-        perror("[-] Connection to the server failed!\n");
+        syslog(LOG_ERR,"[-]Connection to the server failed!\n");        
         close(c_sock_fd);
         exit(1);
     }
-    printf("[+] Connected to the server successfully.\n");
+    printf("[+] Succesfully connected to the TCP Daemon on PORT: %d, address: %d\n",PORT,LOOPBACK_IP);
 
     close(c_sock_fd);
 
