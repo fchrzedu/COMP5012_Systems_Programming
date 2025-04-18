5cdac050b02728389ae7ff58c7233e1e5a19cc10
fchrzedu <19260104@brookes.ac.uk>
2025-04-18 18:39:08 +0100

    modularised sharedlib.c

--- UNIX_DOMAIN_SOCKET_CODE/sharedlib.c
+++ UNIX_DOMAIN_SOCKET_CODE/sharedlib.c
@@ -8,7 +8,7 @@
 #include <syslog.h>
 #include "sharedlib.h"
 
-int handleErr(int fd){
+uint8_t handleErr(int fd){
     close(fd);
     return RES_FAILURE;
 
@@ -36,6 +36,35 @@ void logOpen(){
     openlog(">sharedlib", LOG_PID | LOG_CONS, LOG_USER);
 }
 
+
+/* helper function to send data to daemon down socket */
+ssize_t sendAllData(int fd, const void *d, size_t d_len){
+    ssize_t sent_total = 0;
+    const char *dataptr = (const char*)d;
+    while(sent_total < d_len){
+        ssize_t sent = send(fd, dataptr + sent_total, d_len - sent_total, 0);
+        if(sent<1){
+            syslog(LOG_ERR, "[-sendAllData()] Error sending data to daemon.c");
+            return -1;
+        }
+        sent_total +=sent;
+    }return 0;
+}
+
+ssize_t receiveAllData(int fd, void *buff, size_t length){
+    ssize_t recv_counter = 0;
+    
+    while(recv_counter < length){
+        ssize_t received = recv(fd, (char*)buff + recv_counter, length - recv_counter, 0);
+        if(received < 1){
+            syslog(LOG_ERR, "[-receiveAllData()]Error receiving data\n");
+            return -1;
+        }
+        recv_counter += received;
+    }
+    return recv_counter;
+}
+
 /* Uses op & return codes to return status of block - artibary values*/
 uint8_t sendNewBlock(char *ID, uint8_t *secret, uint32_t data_length, void *data) {
     logOpen();
@@ -45,55 +74,42 @@ uint8_t sendNewBlock(char *ID, uint8_t *secret, uint32_t data_length, void *data
         return RES_FAILURE;
     }
 
-    uint8_t s_cmd= CMD_SEND_BLOCK;
+    uint8_t s_cmd = CMD_SEND_BLOCK;
     int send_block_fd = connectDaemon();
-    uint32_t IDlen = strlen(ID);
+    if (send_block_fd < 0) {
+        return RES_FAILURE;
+    }
 
-    /* sending op code to daemon */
-    if(send(send_block_fd, &s_cmd, sizeof(s_cmd), 0) != sizeof(s_cmd)){
-        syslog(LOG_ERR, "[-]CMD_SEND_BLOCK not sent\n");
+    uint32_t IDlen = strlen(ID) + 1;
+
+    if (sendAllData(send_block_fd, &s_cmd, sizeof(s_cmd)) < 0) {
         return handleErr(send_block_fd);
     }
 
-    /* sending ID - len +1 to account for \0 at end of ID */
-    if(send(send_block_fd, ID, IDlen + 1, 0) != IDlen ){
-        syslog(LOG_ERR, "[-sendnewBlock()]Error sending ID to daemon.c\n");
+    if (sendAllData(send_block_fd, ID, IDlen) != IDlen) {
         return handleErr(send_block_fd);
     }
 
-    /* sending secret - 16 due to the only accepted num of bytes */
-    if(send(send_block_fd, secret, 16, 0) != 16){
-        syslog(LOG_ERR, "[-sendnewBlock()]Error sending secret (16) to daemon.c\n");
+    if (sendAllData(send_block_fd, secret, 16) != 16) {
         return handleErr(send_block_fd);
     }
 
-    /* sending data length over to daemon */
-    if(send(send_block_fd, &data_length, sizeof(data_length), 0) != sizeof(data_length)){
-        syslog(LOG_ERR, "[-sendnewBlock()]Error sending data_length to daemon.c\n");
+    if (sendAllData(send_block_fd, &data_length, sizeof(data_length)) != sizeof(data_length)) {
         return handleErr(send_block_fd);
     }
-    /*
-    - signed counter = don't want 'negative' bytes sent
-    - dataptr points to data
-    - sends data byte by byte, ensures delivery (send() doesn't always send all )*/
-    ssize_t sent_counter = 0;
-    const char *dataptr = (const char *)data;
-    while((uint32_t)sent_counter < data_length){
-        ssize_t sent = send(send_block_fd, dataptr + sent_counter, data_length - sent_counter, 0);
-        if(sent < 1){
-            syslog(LOG_ERR, "[-sendnewBlock()]Error sending data [dataptr] to daemon.c\n");
-            return handleErr(send_block_fd);
-        }
-        sent_counter += sent;
+
+    if (sendAllData(send_block_fd, data, data_length) != data_length) {
+        return handleErr(send_block_fd);
     }
+
     uint8_t resp;
-    if(recv(send_block_fd, &resp, sizeof(resp), 0)!=sizeof(resp)){
-        syslog(LOG_ERR, "[-sendnewBlock()]Error response [recv()] from daemon.c");
+    if (receiveAllData(send_block_fd, &resp, sizeof(resp)) != sizeof(resp)) {
         return handleErr(send_block_fd);
     }
 
     close(send_block_fd);
-    syslog(LOG_INFO,"[+sendNewBlock()] succesful : %d",resp);return resp;
+    syslog(LOG_INFO, "[+]sendNewBlock() successful: %d", resp);
+    return resp;
 
 }
 
@@ -112,66 +128,42 @@ uint8_t getBlock(char *ID, uint8_t *secret, uint32_t buffer_size, void *buffer){
         return RES_FAILURE;
     }
     
-    /* send command over */
     int g_cmd = CMD_GET_BLOCK;
-    if(send(get_blockfd, &g_cmd, sizeof(g_cmd), 0) != sizeof(g_cmd)){
-        syslog(LOG_ERR, "[-getBlock()] CMD_GET_BLOCK send() error\n");
+    if (sendAllData(get_blockfd, &g_cmd, sizeof(g_cmd)) < 0) {
         return handleErr(get_blockfd);
     }
 
-    /* send ID with '\0' */
     uint32_t IDlen = strlen(ID) + 1;
-    if(send(get_blockfd, ID, IDlen, 0)!= IDlen){
-        syslog(LOG_ERR,"[-getBlock()] Error sending ID to daemon.c\n");
-        return handleErr(get_blockfd);
-    }   
-    /* send secret (16 bytes)*/
-    if(send(get_blockfd, secret, 16, 0) != 16){
-        syslog(LOG_ERR,"[-getBlock()] Error sending secret (16) to daemon.c\n");
+    if (sendAllData(get_blockfd, ID, IDlen) != IDlen) {
         return handleErr(get_blockfd);
     }
 
-    /* send buff size */
-    if(send(get_blockfd, &buffer_size, sizeof(buffer_size), 0) != sizeof(buffer_size)){
-        syslog(LOG_ERR,"[-getBlock()] Error sending buffer size to daemon.c\n");
+    if (sendAllData(get_blockfd, secret, 16) != 16) {
         return handleErr(get_blockfd);
     }
 
-    /* retrieve cmd from daemon */
+    if (sendAllData(get_blockfd, &buffer_size, sizeof(buffer_size)) != sizeof(buffer_size)) {
+        return handleErr(get_blockfd);
+    }
+
     uint8_t resp;
-    if(recv(get_blockfd, &resp, sizeof(resp), 0) != sizeof(resp)){
-        syslog(LOG_ERR,"[-getBlock()] Error recv() code from daemon.c\n");
+    if (receiveAllData(get_blockfd, &resp, sizeof(resp)) != sizeof(resp)) {
         return handleErr(get_blockfd);
     }
 
-    if(resp != RES_SUCCESS){
-        syslog(LOG_ERR,"[-getBlock()] daemon.c failed response code = %d\n",resp);
+    if (resp != RES_SUCCESS) {
+        syslog(LOG_ERR, "[-]Daemon failed response code = %d\n", resp);
         close(get_blockfd);
-        return resp; /* could be a different response code each time*/
+        return resp;
     }
 
-    /* recv() data when success */
-    ssize_t recv_counter = 0; 
-    while((uint32_t)recv_counter < buffer_size){
-        ssize_t received = recv(get_blockfd, (char*)buffer + recv_counter, buffer_size - recv_counter, 0);
-
-        /* error check for either incomplete receiving or halt mid process */
-        if(received < 0){
-            syslog(LOG_ERR, "[-getBlock()] Error receiving data from daemon.c\n");
-            return handleErr(get_blockfd);
-        }
-        if(received == 0){
-            syslog(LOG_ERR, "[-getBlock()] Connection terminated!\n");
-            return handleErr(get_blockfd);
-        }
-        recv_counter += received;
+    if (receiveAllData(get_blockfd, buffer, buffer_size) != buffer_size) {
+        return handleErr(get_blockfd);
     }
 
     close(get_blockfd);
-    syslog(LOG_INFO, "[+] getBlock() success. Recieved %d bytes\n",recv_counter);
+    syslog(LOG_INFO, "[+]getBlock() success. Received %d bytes\n", buffer_size);
     return RES_SUCCESS;
-
-
 
 }
 
