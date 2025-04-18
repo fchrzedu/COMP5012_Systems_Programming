d4988756780450ddcd4f8ae2c13423e85995edb4
fchrzedu <19260104@brookes.ac.uk>
2025-04-18 14:53:18 +0100

    Implementing sendNewBlock w/ error checking bytes

--- UNIX_DOMAIN_SOCKET_CODE/sharedlib.c
+++ UNIX_DOMAIN_SOCKET_CODE/sharedlib.c
@@ -1,46 +1,57 @@
 #include <stdio.h>
+
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <syslog.h>
 #include "sharedlib.h"
-
-
-uint8_t sendNewBlock(char *ID, uint8_t *secret, uint32_t data_length, void *data){
-/* send(int sd, const void *msg, size_t msg_len, int flags)
-> sd = sockfd
-> msg = string poiter
-> msg_len = len(msg)
-> flags = 0*/
-    int sockfd = connectDaemon();
-    if(sockfd < 0){return -1;}
-    /* Sending ID */
-    
-    if (send(sockfd, ID, len(ID) + 1, 0) <0){
-        perror("[-]SendID err");
-        close(sockfd);
-        return -1;
-    }
-    
-}
 int connectDaemon(){
-    
     struct sockaddr_un address;
     int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        perror("socket");
+    if(sockfd < 0){
+        syslog(LOG_ERR, "[-]Socket creation failed\n");
         return -1;
     }
 
     memset(&address, 0, sizeof(address));
-    address.sun_family = AF_UNIX;
-    strncpy(address.sun_path, SOCKET_PATH, sizeof(address.sun_path) - 1);
-
-    if (connect(sockfd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("connect");
+    address.sun_family = AD_UNIX;
+    strncpy(address.sun_path, SOCKET_PATH, sizeof(address.sun_path) -1);
+    if(connect(sockfd, (struct sockaddr* )&address, sizeof(address))<0){
+        syslog(LOG_ERR, "[-]Connceting to daemon failed\n");
         close(sockfd);
         return -1;
     }
     return sockfd;
+}
+void openLog(){
+    openlog(">sharedlib", LOG_PID | LOG_CONS, LOG_USER);
+}
+uint8_t sendNewBlock(char *ID, uint8_t *secret, uint32_t data_length, void *data) {
+    /* send()
+    > sd = socked descr
+    > const void *msg = message
+    > size_t = sizeof(*msg)
+    > flags = 0 (always zero)
+    > return = number of bytes actually send - error checking here to make sure complete delivery
+    */
+    openLog();
+    int bsockfd = connectDaemon();
+    if(bsockfd<0){return RES_FAILURE;}
+
+    /* used to let the daemon know of sending datablock - only 1 byte*/
+    uint8_t cmd = CMD_SEND_BLOCK;   
+
+
+    if(send(bsockfd, &cmd, sizeof(cmd), 0) != sizeof(cmd)){
+        syslog(LOG_ERR, "[-]Failed to send cmd_block_send to daemon\n");
+        close(sockfd);
+        return RES_FAILURE;
+    }
+    uint32_t ID_len = strlen(ID) + 1; /* accounts for \0 */
+    if(send(sockfd, ID, ID_len, 0) != ID_len){
+        syslog(LOG_ERR, "[-]ID send error\n");
+    }
+
 }
--- UNIX_DOMAIN_SOCKET_CODE/sharedlib.h
+++ UNIX_DOMAIN_SOCKET_CODE/sharedlib.h
@@ -5,8 +5,20 @@
 
 #define SOCKET_PATH "/tmp/unixdomainsocket"
 
+/* Error return failure codes*/
+#define SEND_BLOCK 1
+#define GET_BLOCK 2
+
+#define RES_SUCCESS      0
+#define RES_FAILURE      1
+#define RES_ACCESS_DENIED 2
+#define RES_NOT_FOUND    3
+#define RES_ALREADY_EXISTS 4
+
 /* Establishes a daemon connection*/
 int connectDaemon();
+
+void openLog();
 
 uint8_t sendNewBlock(char *ID, uint8_t *secret, uint32_t data_length, void *data);
 
